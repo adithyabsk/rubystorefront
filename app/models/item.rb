ce68a1a5d0d13a1800a49c69c3876d35c906a50c
Carl Klier <caklier@ncsu.edu>
2020-02-20 20:54:47 -0500

    added behavior for users

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
 	validates :name, presence: true
 	validates :cost, presence: true
+	validates :brand, presence: true
+	validates :quantity, presence: true
+	validates :imageURL, presence: true
+	validates :category_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,12 @@ class User < ApplicationRecord
     has_secure_password
     has_one_time_password
     validates :name, presence: true
-	validates :email, presence: true
+	validates :email, presence: true, uniqueness: true
 	validates :password, presence: true, if: :password
 	validates :password_confirmation, presence: true, if: :password
 	validates :phone_num, presence: true
 	validates :dob, presence: true
 	validates :address_l1, presence: true
-	validates :address_l2, presence: true
 	validates :city, presence: true
 	validates :state, presence: true
 	validates :zip, presence: true

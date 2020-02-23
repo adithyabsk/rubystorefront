448aef54d77587b3184cdcbc2a222059c75e79fd
Carl Klier <caklier@ncsu.edu>
2020-02-22 22:43:19 -0500

    added UserMailer to address issue #27

--- app/mailers/subscribe_mailer.rb
+++
@@ -1,7 +0,0 @@
-class SubscribeMailer < ApplicationMailer
-	default from: 'csc517store@gmail.com'
-	def subscribe_email
-		@user = current_user
-		mail(to: @user.email, subject: "Item Now Available")
-	end
-end

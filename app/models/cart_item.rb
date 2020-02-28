71e4a08839d3d640571994361b1047c07e83c8dc
cmfoley2 <cmfoley2@ncsu.edu>
2020-02-28 14:31:13 -0500

    Adding senior discount

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,7 +2,15 @@ class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :cart
 
-  def total_cost
+  def sub_total_cost
     self.quantity * self.item.cost
+  end
+  
+  def tax
+	self.quantity * self.item.cost * Category.find(self.item.category_id).tax_slab
+  end
+  
+  def total_cost
+	self.sub_total_cost + self.tax
   end
 end

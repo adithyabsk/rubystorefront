669a65748edf74ef151917c7dba27c6b9f300f98
Adithya Balaji <abalaji2@ncsu.edu>
2020-02-29 03:40:18 -0500

    Add aasm based management of use returns and restricted item purchase flows.

--- app/models/ledger_entry.rb
+++ app/models/ledger_entry.rb
@@ -1,50 +1,37 @@
-LedgerStatus = Struct.new(:name, :user_action, :admin_action, :action_type, :next_state) do
-  def action_type_match?(action_check)
-    action_type == action_check
-  end
-  def should_disable?(is_admin)
-    if (is_admin and not action_type_match?(:admin_action)) \
-        or (not is_admin and not action_type_match?(:user_action)) \
-        or action_type_match?(:neither_action)
-      return true
-    else
-      return false
-    end
-  end
-  def action_string(is_admin)
-    if is_admin
-      admin_action
-    else
-      user_action
-    end
-  end
-end
-
-ORDER_STATUS = {
-    :purchased => LedgerStatus.new("Purchased", "Request Return", "None", :user_action, :return_requested),
-    :return_requested =>  LedgerStatus.new("Return Requested", "Request Return", "Approve Return", :admin_action, :return_approved),
-    :return_approved =>  LedgerStatus.new("Return Approved", "Request Return", "Approve Return", :neither_action, :no_state),
-    :approval_requested =>  LedgerStatus.new("Approval Requested", "Request Approval", "Approve Request", :admin_action, :order_approved),
-    :order_approved =>  LedgerStatus.new("Order Approved", "Request Return", "Approve Request", :user_action, :return_requested)
-}.freeze
-
 class LedgerEntry < ApplicationRecord
+  include AASM
 
   belongs_to :user
   belongs_to :item
 
-  validates :status, presence: true, inclusion: {
-      in: ORDER_STATUS.keys.map{ |k| k.to_s },
-      message: "%{value} is not a valid status %{}"
-  }
+  aasm column: :status do
+    state :ordered, :return_requested, :return_approved, :approval_requested, :order_approved
+    initial_state lambda { |ledger_entry|
+      ledger_entry.item.restricted? ? :approval_requested : :ordered
+    }
 
-  def status_data
-    return ORDER_STATUS[self.status.to_sym]
+    event :request_return, guard: Proc.new {|cu| is_user_transition?(cu) } do
+      transitions from: [:ordered, :order_approved], to: :return_requested
+    end
+
+    event :approve_return, guard: Proc.new {|cu| cu.is_admin? }  do
+      transitions from: :return_requested, to: :return_approved
+    end
+
+    event :approve_restricted, guard: Proc.new {|cu| cu.is_admin? }  do
+      transitions from: :approval_requested, to: :order_approved
+    end
   end
 
-  def set_next_state!
-    if self.status_data.next_state != :no_state
-      self.update(status: self.status_data.next_state)
-    end
+  def is_user_transition?(current_user)
+    is_user?(current_user) or is_admin_ledger?
+  end
+
+  def is_user?(current_user)
+    not current_user.is_admin?
+  end
+
+  def is_admin_ledger?
+    user.is_admin?
   end
 end

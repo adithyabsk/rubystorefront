9f01bd55908791e01b00d1c1141a62849e850403
Carl Klier <caklier@ncsu.edu>
2020-03-01 00:30:35 -0500

    add email to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries=true
+  config.action_mailer.default_url_options = {:host => 'desolate-lowlands-58665.herokuapp.com/', protocol: 'https'}
+  config.action_mailer.smtp_settings = {
+          address: 'smtp.gmail.com',
+          port: 587,
+          domain: 'csc517store.com',
+          user_name: 'csc517store@gmail.com',
+          password: 'kcjboadznetzxaij',
+          authentication: 'plain'
+  }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
